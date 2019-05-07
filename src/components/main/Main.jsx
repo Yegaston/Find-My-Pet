879dad8680c6665a5aa8c5cc2802fb520c477bb4
Gaston Graciani <Gastongraciani@gmail.com>
2019-05-06 23:54:59 -0300

    Added dropdown with sign out posibility

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -35,18 +35,23 @@ function ItemsNavWhenNoAuth() {
     )
 }
 
-function ItemsNavWhenAuth() {
+function ItemsNavWhenAuth(props) {
     return (
-        <div className="collapse navbar-collapse" id="navbarSupportedContent-555">
-            <ul className="navbar-nav ml-auto">
-                <li className="nav-item active">
+        <div className="collapse navbar-collapse container" id="navbarSupportedContent-555">
+            <ul className="navbar-nav ml-auto ml-5">
+                <li className="nav-item">
                     <Link to='/' className="nav-link">Feed</Link>
                 </li>
-                <li className="nav-item active">
+                <li className="nav-item">
                     <Link to='/lostFavs' className="nav-link">Losts Pets</Link>
                 </li>
-                <li className="nav-item active">
-                    <Link to='/userSettings' className="nav-link">Account</Link>
+                <li className="nav-item dropdown">
+                    <i className="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">Account Settings</i>
+                    <div className="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+                        <Link to='/userSettings' className="dropdown-item">Account</Link>
+                        <button className="dropdown-item" onClick={props.signOut}>Sign Out</button>
+                    </div>
                 </li>
             </ul>
         </div>
@@ -87,17 +92,17 @@ class Navbar extends Component {
                     aria-controls="navbarSupportedContent-555" aria-expanded="false" arial-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                {this.state.authenticated ? <ItemsNavWhenAuth /> : <ItemsNavWhenNoAuth />}
+                {this.state.authenticated ? <ItemsNavWhenAuth signOut={this.props.signOut} /> : <ItemsNavWhenNoAuth />}
             </nav>
         )
     }
 }
 
-function Navigation() {
+function Navigation(props) {
     return (
         <Router>
             <div>
-                <Navbar />
+                <Navbar signOut={props.signOut} />
             </div>
             <Route path="/" exact component={Index} />
             <Route path="/login/" component={Login} />
@@ -194,10 +199,22 @@ class Login extends Component {
 }
 
 export default class Main extends Component {
+    constructor() {
+        super()
+        this.signOut = this.signOut.bind(this)
+    }
 
+    async signOut() {
+        try {
+            await auth().signOut()
+            console.log("Out")
+        } catch (err) {
+            console.log("Error")
+        }
+    }
     render() {
         return (
-            <Navigation />
+            <Navigation signOut={this.signOut} />
         )
     }
 }
