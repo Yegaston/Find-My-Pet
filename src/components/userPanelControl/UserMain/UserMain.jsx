d11c37eb617467c1c2fef8873d67f3c6c48168e3
Gaston Graciani <Gastongraciani@gmail.com>
2019-05-09 17:17:24 -0300

    Added some things in UserMain

--- src/components/userPanelControl/UserMain/UserMain.jsx
+++ src/components/userPanelControl/UserMain/UserMain.jsx
@@ -1,22 +1,53 @@
 import React, { Component } from 'react';
-import db from '../../../services/db-user';
+import dbUser from '../../../services/db-user';
 
+import Loading from '../../loading/Loading';
+import { auth } from 'firebase';
 
 export default class UserMain extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            email: this.props.email,
-            user: {}
+            email: '',
+            user: {},
+            loading: true,
+            authenticated: false,
         }
+        this.isAuthThisUser = this.isAuthThisUser.bind(this);
+        this.getUserData = this.getUserData.bind(this);
     }
 
+
+    isAuthThisUser() {
+        auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loading: false, authenticated: true, email: user.email });
+                console.log("Auth");
+
+            } else {
+                this.setState({ loading: false, authenticated: false });
+                console.log("No Auth");
+            }
+        });
+    }
+
+
     async getUserData() {
+        this.isAuthThisUser();
+        console.log(this.state.email);
         try {
-            const user = await db.doc(this.state.email).get()
+            const user = await dbUser.doc(this.state.email).get()
             this.setState({
-                user: user.data()
+                user: {
+                    email: user.data().email,
+                    postalCode: user.data().postalCode,
+                    address: user.data().address,
+                    country: user.data().country,
+                    name: user.data().name,
+                    phone: user.data().phone,
+                    state: user.data().state,
+                }
             });
             console.log(this.state.user)
         }
@@ -32,7 +63,12 @@ export default class UserMain extends Component {
     render() {
         return (
             <div>
-                <UserAvatar email={this.state.email} user={this.state.user}  />
+                {this.state.loading ? <Loading /> : 
+                <UserAvatar
+                    email={this.state.user.email}
+                    name={this.state.user.name}
+                    postalcode={this.state.postalCode}
+                />}
             </div>
         )
     }
@@ -40,19 +76,19 @@ export default class UserMain extends Component {
 
 function UserAvatar(props) {
     return (
-        <div className="container">
+        <div className="m-3">
             <div className="row mt-5">
-            <div className="col-md-2">
-                <h2>Lateral Bar</h2>
-            </div>
+                <div className="col-md-2">
+                    <h2>Lateral Bar</h2>
+                </div>
                 <div className="col-md-4 ">
                     <img className="img-fluid" src="https://avatars0.githubusercontent.com/u/39235181?s=460&v=4" alt="Profile ge" />
                 </div>
                 <div className="div col-md-4">
-                    <h4>{props.user.name}</h4>
+                    <h4>{props.name}</h4>
                     <h5 className="subname">{props.email}</h5>
-                    <h5 className="subname">{props.user.state}</h5>
-                    <h5 className="subname">Postal Code: {props.user.postalcode}</h5>
+                    <h5 className="subname">{props.name}</h5>
+                    <h5 className="subname">Postal Code: {props.postalcode}</h5>
                 </div>
             </div>
         </div>
